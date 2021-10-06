793ba3436e1658cb6374b37c74390901cb738d4b
Jakub Parlej <jakubparlej@gmail.com>
2021-10-06 21:07:57 +0200

    Added project section

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  spaceBetween: 16,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: "#next",
+    prevEl: "#prev",
+  },
+});
