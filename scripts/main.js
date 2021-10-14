6740f60713b0d98785df06d8e6989d238ae9d766
Jakub Parlej <jakubparlej@gmail.com>
2021-10-14 22:20:13 +0200

    Add contact section

--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,47 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const rootElement = document.querySelector("#root");
-  const sections = document.querySelectorAll("section");
-  // const footer = document.querySelector("footer");
-  let currentSectionIndex = 0;
-  let isThrottled = false;
+// document.addEventListener("DOMContentLoaded", () => {
+//   const rootElement = document.querySelector("#root");
+//   const sections = document.querySelectorAll("section");
+//   // const footer = document.querySelector("footer");
+//   let currentSectionIndex = 0;
+//   let isThrottled = false;
 
-  sections[currentSectionIndex].scrollIntoView({
-    behavior: "smooth",
-    block: "start",
-  });
+//   sections[currentSectionIndex].scrollIntoView({
+//     behavior: "smooth",
+//     block: "start",
+//   });
 
-  document.addEventListener("wheel", (event) => {
-    if (isThrottled) return;
-    isThrottled = true;
+//   document.addEventListener("wheel", (event) => {
+//     if (isThrottled) return;
+//     isThrottled = true;
 
-    setTimeout(() => {
-      isThrottled = false;
-    }, 500);
+//     setTimeout(() => {
+//       isThrottled = false;
+//     }, 500);
 
-    const direction = event.deltaY > 0 ? 1 : -1;
+//     const direction = event.deltaY > 0 ? 1 : -1;
 
-    if (direction === 1) {
-      const isLastSection = currentSectionIndex === sections.length - 1;
-      if (isLastSection) return;
-    } else if (direction === -1) {
-      const firstSection = currentSectionIndex === 0;
-      if (firstSection) return;
-    }
+//     if (direction === 1) {
+//       const isLastSection = currentSectionIndex === sections.length - 1;
+//       if (isLastSection) return;
+//     } else if (direction === -1) {
+//       const firstSection = currentSectionIndex === 0;
+//       if (firstSection) return;
+//     }
 
-    currentSectionIndex = currentSectionIndex + direction;
+//     currentSectionIndex = currentSectionIndex + direction;
 
-    sections[currentSectionIndex].scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
+//     sections[currentSectionIndex].scrollIntoView({
+//       behavior: "smooth",
+//       block: "start",
+//     });
 
-    // if (currentSectionIndex === sections.length - 1) {
-    //   footer.classList.add("show");
-    // } else {
-    //   footer.classList.remove("show");
-    // }
-  });
-});
+//     // if (currentSectionIndex === sections.length - 1) {
+//     //   footer.classList.add("show");
+//     // } else {
+//     //   footer.classList.remove("show");
+//     // }
+//   });
+// });
 
 const swiper = new Swiper(".swiper", {
   // Optional parameters
